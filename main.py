71182a4052f11d9024c654ef5cba199ff741e7d5
kellandrosu <kellandr@oregonstate.edu>
2018-04-19 16:06:33 -0700

    impliment PATCH /boat/:id

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class Boat(ndb.Model):
     at_sea = ndb.BooleanProperty(default=True)
 
 
+#create new boat
 @app.route('/boats', methods=["POST"])
 def createBoat():
 
@@ -56,15 +57,35 @@ def createBoat():
 
 	return resp
 
-@app.route('/boats/<boatId>', methods=['GET'])
+
+#get or modify boat by id
+@app.route('/boats/<boatId>', methods=['GET', 'PATCH'])
+#@app.route('/boats/<boatId>/', methods=['GET', 'PATCH'])
 def getBoat(boatId):
 
 	boatkey = ndb.Key(urlsafe=boatId)
 	boat = boatkey.get()
 
-	if boat != None :
+	if boat != None :	
+
+		if request.method == 'PATCH' :
+			reqObj = request.get_json(force=True)
+			#iterate through
+			print reqObj
+			if 'name' in reqObj :
+				boat.name = reqObj['name']
+			if 'type' in reqObj :
+				boat.type = reqObj['type']
+			if 'length' in reqObj:
+				boat.length = reqObj['length']
+			if 'at_sea' in reqObj :
+				boat.at_sea = reqObj['at_sea']
+
+			boat.put()
+
 		payload = boatToJson(boat)
 		status_code = 200
+
 	else:
 		status_code = 400
 		payload = { message: "Error: Could not find Boat with id " + boatId }
@@ -75,6 +96,7 @@ def getBoat(boatId):
 	
 	return resp
 	
+#get all boats
 @app.route('/boats', methods=['GET'])
 @app.route('/boats/', methods=['GET'])
 def getBoats():
@@ -91,6 +113,8 @@ def getBoats():
 	
 	return resp
 
+
+#-----------------  Helper Functions  ------------------
 
 def boatToJson(boat):
 	payload = {
