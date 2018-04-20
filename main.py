006d39d4ebac0dd1ad3b2d9a247671db9a2580e1
kellandrosu <kellandr@oregonstate.edu>
2018-04-20 14:49:56 -0700

    impliments: POST /slip ,  DELETE /all

--- main.py
+++ main.py
@@ -7,22 +7,73 @@ from flask import Flask, render_template, request, jsonify, json
 
 app = Flask(__name__)
 
+#set print to console
 logging.basicConfig(level=logging.DEBUG)
 
-#use flask to handle requests. 
-	#interpret request body using json
+#disables strict slashes GLOBALLY https://stackoverflow.com/questions/33241050/trailing-slash-triggers-404-in-flask-path-rule?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+app.url_map.strict_slashes = False
+
+
 
 #boat entity definition
 class Boat(ndb.Model):
-#    id = ndb.StringProperty()
     name = ndb.StringProperty()
     type = ndb.StringProperty()
     length = ndb.FloatProperty()
     at_sea = ndb.BooleanProperty(default=True)
 
+class Departure(ndb.Model):
+	date = ndb.DateProperty()
+	boat = ndb.KeyProperty(Boat)
+
+class Slip(ndb.Model):
+	number = ndb.IntegerProperty()
+	current_boat = ndb.KeyProperty(Boat)
+	arrival_date = ndb.DateProperty()
+	#departure_date = ndb.StructuredProperty(Departure, repeated=True)
+
+#manages slip human-readable numbers 
+class SlipNum(ndb.Model):
+	number = ndb.IntegerProperty()
+	taken = ndb.BooleanProperty()
+
+
+#   ----------  REQUEST HANDLERS  ------------
+
+@app.route('/all', methods=['DELETE'])
+def deleteAll():
+
+	allKeys = Boat.query().fetch(keys_only=True)
+	allKeys.extend(Departure.query().fetch(keys_only=True))
+	allKeys.extend(Slip.query().fetch(keys_only=True))
+	allKeys.extend(SlipNum.query().fetch(keys_only=True))
+	ndb.delete_multi( allKeys )
+	return 'All Entities Removed'
+
+
+
+@app.route('/slips', methods=['POST'])
+def createSlip():
+
+	slip = Slip()		
+
+	assignSlipNum(slip)
+	slip.put()
+
+	resp = jsonify( { 
+		"id": slip.key.urlsafe(), 
+		"number" : slip.number,
+		"current_boat" : slip.current_boat,
+		"arrival_date" : slip.arrival_date 
+	} )
+
+	resp.status_code = 200
+	return resp
+
+
 
 #create new boat
-@app.route('/boats', methods=["POST"])
+@app.route('/boats', methods=['POST'])
 def createBoat():
 
 	reqObj = request.get_json(force=True)
@@ -32,11 +83,9 @@ def createBoat():
 		boat = Boat(
 	    	name=reqObj['name'],
 	    	type=reqObj['type'],
-	    	length=reqObj['length']
+	    	length=reqObj['length'],
+	    	at_sea=True
 	    	)
-
-		if 'at_sea' in reqObj :
-			boat.at_sea = reqObj['at_sea']
 
 		#write boat to ndb
 		boatkey = boat.put()
@@ -58,9 +107,8 @@ def createBoat():
 	return resp
 
 
-#get or modify boat by id
+#get, modify or delete boat by id
 @app.route('/boats/<boatId>', methods=['GET', 'PATCH', 'DELETE'])
-@app.route('/boats/<boatId>/', methods=['GET', 'PATCH', 'DELETE'])
 def getBoat(boatId):
 
 	boatkey = ndb.Key(urlsafe=boatId)
@@ -76,7 +124,9 @@ def getBoat(boatId):
 
 		#handle GET and PATCH
 		else :
+
 			if request.method == 'PATCH' :
+				print 'here'
 				reqObj = request.get_json(force=True)
 				#iterate through
 				print reqObj
@@ -105,9 +155,9 @@ def getBoat(boatId):
 	
 	return resp
 	
+
 #get all boats
 @app.route('/boats', methods=['GET'])
-@app.route('/boats/', methods=['GET'])
 def getBoats():
 
 	qry = Boat.query()
@@ -125,6 +175,34 @@ def getBoats():
 
 #-----------------  Helper Functions  ------------------
 
+#finds or creates available slip number and updates slip with new number
+def assignSlipNum(slip):
+
+	qry = SlipNum.query()
+
+	if not qry.fetch():
+		#if SlipNum is empty, create first number, add to set and update slip
+		slipNum = SlipNum(number=1, taken=True)
+
+	else:
+		availableNums = qry.filter(SlipNum.taken==False)
+		
+		if not availableNums.fetch():
+			#create new SlipNum from largest in qry + 1
+			newNumber = qry.order(-SlipNum.number).fetch()[0].number + 1
+			slipNum = SlipNum(number=newNumber, taken=True)
+
+		else:
+			#get lowest available and assign it
+			slipNum = availableNums.order(SlipNum.number).iter().next()
+			slipNum.taken = True
+	
+	print 'assigning slip number ' + str(slipNum.number)
+	slipNum.put()
+	slip.number = slipNum.number
+
+
+#returns json object of given Boat class
 def boatToJson(boat):
 	payload = {
 			'name' : boat.name,
